fe3b2e6cee8cb3abdd0626ffe95209252138c150
Jayant Kumbhar <jayant.ska@gmail.com>
2020-01-14 12:03:07 +0530

    story_AT1-365: Add hdbpp viewer scripts.

--- docker/tango/tango-archiver/test/SimpleTest.py
+++ docker/tango/tango-archiver/test/SimpleTest.py
@@ -3,10 +3,10 @@ import time
 
 def test_cm_device_is_ON():             #Test Case for config manager
     tango_test = tango.DeviceProxy("archiving/hdbpp/confmanager01")
-    time.sleep(120)
+    time.sleep(2)
     assert tango_test.state() == tango.DevState.ON
 
 def test_es_device_is_ON():             #Test Case for Event subscriber
     tango_test = tango.DeviceProxy("archiving/hdbpp/eventsubscriber01")
-    time.sleep(120)
+    time.sleep(2)
     assert tango_test.state() == tango.DevState.ON
