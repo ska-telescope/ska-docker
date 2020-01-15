da41515ad76f83d45c85a73e721ad5f0436af91f
Digvijay Mali <digvijay.ska@gmail.com>
2020-01-15 11:47:51 +0530

    story_AT1-365:

--- docker/tango/tango-archiver/test/SimpleTest.py
+++ docker/tango/tango-archiver/test/SimpleTest.py
@@ -1,12 +1,12 @@
 import tango
 import time
 
-def test_cm_device_is_ON():             #Test Case for config manager
+def test_cm_device_is_ON():
     tango_test = tango.DeviceProxy("archiving/hdbpp/confmanager01")
     time.sleep(2)
     assert tango_test.state() == tango.DevState.ON
 
-def test_es_device_is_ON():             #Test Case for Event subscriber
+def test_es_device_is_ON():
     tango_test = tango.DeviceProxy("archiving/hdbpp/eventsubscriber01")
     time.sleep(2)
     assert tango_test.state() == tango.DevState.ON
